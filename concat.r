9acc1aa10023a5b95043878a9b7ced05bc9adf9e
flixofarrell <flix.ofarrell@gmail.com>
2020-05-07 01:03:52 +0100

    time efficient table handling

--- concat.r
+++ concat.r
@@ -1,16 +1,27 @@
+#Concatenater: 
+#Concatenate dataframe to master dataframe
+#Needs to be executed with one thread to make sure
+#no rows are duplicated/misssed out.
+
+#Developer: Felix O'Farrell
+#May 2020
+
+
 library('data.table')
 require(data.table)
 
+#command line args to plug into pipe
 args <- commandArgs(trailingOnly=TRUE)
 
+#read in master
+m_dt <- fread(args[1])
 
-system.time(m_dt <- fread(args[1]))
+#read in Chrx+1 .fastgwa file
+n_dt <- fread(args[2])
 
-system.time(n_dt <- fread(args[2]))
+#concat Chrx+1 to master 
+m_dt <- merge.data.table(m_dt, n_dt, all = TRUE)
 
-
-m_dt <- merge(m_dt, n_dt, all = TRUE)
-
-
-
+#write to master file
 fwrite(m_dt, args[1])
+
